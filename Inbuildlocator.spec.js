293f6962491de364df0338b2432c7ba5e8105968
Ra-tester <rpoulose369@gmail.com>
2025-03-12 11:53:50 +1300

    Update Inbuildlocator.spec.js

--- Inbuildlocator.spec.js
+++ Inbuildlocator.spec.js
@@ -4,7 +4,7 @@ page.getBylabel() to locate a form control by associated label's text
 page.getByplaceholder() to locate an input by palceholder 
 page.getByAltText() to locate an element, usually image, by itz text alternative
 page.getByTitle() to locate an element by it's title attribute 
-page.getByTestId() to locate an element based on it's data testid attribute*
+page.getByTestId() to locate an element based on it's data testid attribute*/
 
 
 const {test,expect} = require('@playwright/test')
@@ -17,7 +17,7 @@ await expect(logo).toBeVisible();
 
 })*/
 
-/*const {test,expect} = require('@playwright/test')
+const {test,expect} = require('@playwright/test')
 test ('placeholder', async({page})=> {
 
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
